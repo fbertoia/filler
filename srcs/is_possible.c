e283672c4365ddc484e88565c2256dfaee1291bd
jcamhi <jeremshy@h3lp.fr>
2018-01-20 19:07:33 +0100

    Rainbow Dash is best pony !

--- srcs/is_possible.c
+++ srcs/is_possible.c
@@ -1,10 +1,9 @@
 #include <filler.h>
 
-
 static int	point_is_possible(t_d *data, int x, int y)
 {
-	x = get_coord_board(x, data->size_x);
-	y = get_coord_board(y, data->size_y);
+	if (x < 0 || y < 0 || x >= data->size_x || y >= data->size_y)
+		return (-1);
 	if (data->board[x][y] == data->enemy_char)
 		return (-1);
 	if (data->board[x][y] == data->ally_char)
