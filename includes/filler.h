b7d5d8118aabad85d0a882bc5063e1fb22ba194f
jcamhi <jeremshy@h3lp.fr>
2018-01-22 18:49:41 +0100

    norme ok !

--- includes/filler.h
+++ includes/filler.h
@@ -5,14 +5,14 @@
 # include <stdio.h>
 # include <limits.h>
 
-enum e_flag
+enum			e_flag
 {
 	state_first_line,
 	state_board,
 	state_piece
 };
 
-typedef struct s_p
+typedef struct	s_p
 {
 	struct s_p	*next;
 	int			distance;
@@ -20,7 +20,7 @@ typedef struct s_p
 	int			y;
 }				t_p;
 
-typedef struct s_vec
+typedef struct	s_vec
 {
 	float	x;
 	float	y;
@@ -57,49 +57,49 @@ typedef struct	s_gen
 	struct s_gen	*next;
 }				t_gen;
 
-typedef int (*t_func)(void *a, void *b);
+typedef int		(*t_func)(void *a, void *b);
 
-int		parse_first_line(char *line, char *av);
-int		parse_board(char *line, t_d *data);
-int		parse_piece(char *line, t_d *data);
-int		cmp(char *line, int *i, char c);
+int				parse_first_line(char *line, char *av);
+int				parse_board(char *line, t_d *data);
+int				parse_piece(char *line, t_d *data);
+int				cmp(char *line, int *i, char c);
 
-int		delboard(t_d *data);
-int		delpiece(t_d *data);
-void	dellist(t_p **list);
-int		par(int ret, char *str);
+int				delboard(t_d *data);
+int				delpiece(t_d *data);
+void			dellist(t_p **list);
+int				par(int ret, char *str);
 
-void		*insert_sort(void *list, t_func comp);
-int			func(void *a, void *b);
+void			*insert_sort(void *list, t_func comp);
+int				func(void *a, void *b);
 
-void	print_board(char **board);
-void	print_list(t_p *list);
-void	print_board_with_piece(t_d *data, int x, int y);
-void	print_piece(char **piece);
+void			print_board(char **board);
+void			print_list(t_p *list);
+void			print_board_with_piece(t_d *data, int x, int y);
+void			print_piece(char **piece);
 
-int		is_possible(t_d *data, int x, int y);
+int				is_possible(t_d *data, int x, int y);
 
-t_p	*list_possible_pieces(t_d *data);
+t_p				*list_possible_pieces(t_d *data);
 
-int		calculate_distance_piece(t_d *data, int x, int y);
-int		calculate_distance(t_p *point, t_p *target);
+int				calculate_distance_piece(t_d *data, int x, int y);
+int				calculate_distance(t_p *point, t_p *target);
 
-t_p		*create_point(int x, int y, int distance);
-int		add_new_point(t_p **points, int x, int y);
-void	add_point(t_p **list, t_p *point);
+t_p				*create_point(int x, int y, int distance);
+int				add_new_point(t_p **points, int x, int y);
+void			add_point(t_p **list, t_p *point);
 
-int		put_piece(t_d *data);
+int				put_piece(t_d *data);
 
-int get_coord_board(int val, int max);
+int				get_coord_board(int val, int max);
 
-int		func_bis(void *a, void *b);
-int		calculate_proximity(t_p *point, t_d *data);
-int		calculate_cross(t_d *data, int x, int y);
+int				func_bis(void *a, void *b);
+int				calculate_proximity(t_p *point, t_d *data);
+int				calculate_cross(t_d *data, int x, int y);
 
 int				calculate_proximity(t_p *point, t_d *data);
 
 int				parse_2(t_d *data);
-int		parse_board_line(char *line, t_d *data);
-int			parse_board_other(t_d *data);
+int				parse_board_line(char *line, t_d *data);
+int				parse_board_other(t_d *data);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,79 +1,93 @@
 #include <filler.h>
 #include <unistd.h>
 
-int main(int ac, char **av)
+void	init_data(t_d *data, char *av)
+{
+	data->size_x = -1;
+	data->piece_x = -1;
+	data->size_y = -1;
+	data->piece_y = -1;
+	data->board = NULL;
+	data->piece = NULL;
+	data->first_round = 1;
+	data->av = av;
+	data->points = NULL;
+	data->enemy_points = NULL;
+	data->log_fd = open("./log", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	data->debug_fd = open("./debug", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	data->target.next = NULL;
+	data->nbr_tours = 0;
+	data->touched_enemy = 0;
+}
+
+int		state_fl(t_d *data, int *state, char *s)
+{
+	data->player_number = parse_first_line(s, data->av);
+	if (data->player_number < 0)
+	{
+		ft_putstr_fd("Parsing error : Invalid first line\n", 2);
+		return (0);
+	}
+	if (data->player_number == 1)
+	{
+		data->ally_char = 'O';
+		data->enemy_char = 'X';
+	}
+	else
+	{
+		data->ally_char = 'X';
+		data->enemy_char = 'O';
+	}
+	*state = state_board;
+	return (1);
+}
+
+int		state_b(t_d *data, char *s, int *state)
+{
+	if (parse_piece(s, data) <= 0)
+		return (0);
+	if (!put_piece(data))
+	{
+		delboard(data);
+		delpiece(data);
+		ft_memdel((void**)&s);
+		dellist(&(data->points));
+		return (0);
+	}
+	delpiece(data);
+	data->piece_x = -1;
+	data->piece_y = -1;
+	data->first_round = 0;
+	*state = state_board;
+	dellist(&(data->enemy_points));
+	data->nbr_tours++;
+	return (1);
+}
+
+int		main(int ac, char **av)
 {
 	char	*s;
 	t_d		data;
 	int		state;
 
-	if (ac != 1)
-		return (1);
-	data.size_x = -1;
-	data.piece_x = -1;
-	data.size_y = -1;
-	data.piece_y = -1;
-	data.board = NULL;
-	data.piece = NULL;
-	data.first_round = 1;
-	data.av = av[0];
-	data.points = NULL;
-	data.enemy_points = NULL;
-	data.log_fd = open("./log", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	data.debug_fd = open("./debug", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	data.target.next = NULL;
+	(void)ac;
+	init_data(&data, av[0]);
 	state = state_first_line;
-	data.nbr_tours = 0;
-	data.touched_enemy = 0;
 	while ((get_next_line(0, &s)) > 0)
 	{
-		dprintf(data.log_fd, "%s\n", s);
 		if (state == state_first_line)
 		{
-			data.player_number = parse_first_line(s, av[0]);
-			if (data.player_number < 0)
-			{
-				ft_putstr_fd("Parsing error : Invalid first line\n", 2);
+			if (!state_fl(&data, &state, s))
 				return (1);
-			}
-			if (data.player_number == 1)
-			{
-				data.ally_char = 'O';
-				data.enemy_char = 'X';
-			}
-			else
-			{
-				data.ally_char = 'X';
-				data.enemy_char = 'O';
-			}
-			state = state_board;
 		}
 		else if (state == state_board)
 		{
-			if (parse_board(s, &data) <= 0)
+			if (((state = state_piece) || 1) && parse_board(s, &data) <= 0)
 				return (2);
-			state = state_piece;
 		}
 		else if (state == state_piece)
-		{
-			if (parse_piece(s, &data) <= 0)
+			if (!state_b(&data, s, &state))
 				return (3);
-			if (!put_piece(&data))
-			{
-				delboard(&data);
-				delpiece(&data);
-				ft_memdel((void**)&s);
-				dellist(&(data.points));
-				return (4);
-			}
-			delpiece(&data);
-			data.piece_x = -1;
-			data.piece_y = -1;
-			data.first_round = 0;
-			state = state_board;
-			dellist(&(data.enemy_points));
-			data.nbr_tours++;
-		}
 		ft_memdel((void**)&s);
 	}
 	return (0);
